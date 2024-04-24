532bb5a3bf031f3c6f02befc4adf04d3c8a6b319
Mo <mohamedabdelhamid3030@gmail.com>
2024-04-24 21:46:55 +0200

    update resource tracker

--- src/train.py
+++ src/train.py
@@ -10,7 +10,7 @@ from utils import (
     read_csv_in_directory,
     read_json_as_dict,
     set_seeds,
-    TimeAndMemoryTracker,
+    ResourceTracker,
 )
 
 logger = get_logger(task_name="train")
@@ -42,7 +42,7 @@ def run_training(
     """
 
     try:
-        with TimeAndMemoryTracker(logger) as _:
+        with ResourceTracker(logger, monitoring_interval=0.1):
             logger.info("Starting training...")
             # load and save schema
             logger.info("Loading and saving schema...")
@@ -93,7 +93,6 @@ def run_training(
         log_error(message=err_msg, error=exc, error_fpath=paths.TRAIN_ERROR_FILE_PATH)
         # re-raise the error
         raise Exception(f"{err_msg} Error: {str(exc)}") from exc
-
 
 
 if __name__ == "__main__":
